b422602e9c6e61eec63ae3a5d9f7f0f3a223da62
Okinz <steven14812@gmail.com>
2020-10-01 02:20:24 -0400

    Created component folder and moved app.js file

---
+++ src/components/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import NavBar from "./NavBar";
+import LandingPage from "./LandingPage";
+import AboutMe from "./AboutMe";
+import Skills from "./Skills";
+import Projects from "./Projects";
+import Footer from "./Footer";
+import '../App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <NavBar />
+        <LandingPage />
+        <AboutMe />
+        <Skills />
+        <Projects />
+        <Footer />
+      </header>
+    </div>
+  );
+}
+
+export default App;
