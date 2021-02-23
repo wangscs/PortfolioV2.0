f97914190fd4516ccd13d8e9fe0ae673769f8bc8
Okinz <steven14812@gmail.com>
2021-02-23 01:17:09 -0500

    Added scroll to hide/show function for navbar

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -2,13 +2,15 @@ import React from "react";
 
 function LandingPage() {
   return (
-    <div className="landing-page">
-      <h3>Hi my name is </h3>
-      <h1>Steven Wang</h1>
-      <p>
-        I am a software engineer specialized in building and creating 
-        exceptional web applications.
-      </p>
+    <div className="landing-page-container">
+      <div className="landing-page">
+        <h3>Hi my name is </h3>
+        <h1>Steven Wang</h1>
+        <p>
+          I am a software engineer specialized in building and creating 
+          exceptional web applications.
+        </p>
+      </div>
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,15 +1,38 @@
-import React from "react";
-// import { BrowserRouter, Route, Link } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { debounce} from '../utilities/helpers'
 
-function NavBar() {
+function NavBar(){
+  const [prevScrollPos, setPrevScrollPos] = useState(0);
+  const [visible, setVisible] = useState(true);
+
+  const handleScroll = debounce(()=> {
+    // find current scroll position
+    const currentScrollPos = window.pageYOffset;
+    
+    // set state based on location info
+    setVisible((prevScrollPos > currentScrollPos && prevScrollPos - currentScrollPos > 60) || currentScrollPos < 10);
+
+    // set state to new scroll position
+    setPrevScrollPos(currentScrollPos);
+  }, 100);
+  
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    
+    return () => window.removeEventListener('scroll', handleScroll);
+
+  }, [prevScrollPos, visible, handleScroll]);
+
   return (
 
-    <div>
+    <div className="nav-bar-container" style={{top: visible ? '0' : '-80px'}}>
       <ul className="navBar">
         <li><a href="/" className="logo">Home</a></li>
         <li><a href="/about">About</a></li>
         <li><a href="/skills">Skills</a></li>
         <li><a href="/projects">Projects</a></li>
+        <li><a href="/contact">Contact</a></li>
+        <li><a href="/resume" className="resume">Resume</a></li>
       </ul>
     </div>
   );
