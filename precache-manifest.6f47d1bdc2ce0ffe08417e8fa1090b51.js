cf6626e028af1e61b3159cfe20dd461746084d3d
Okinz <steven14812@gmail.com>
2021-02-28 04:25:15 -0500

    Updates

---
+++ precache-manifest.6f47d1bdc2ce0ffe08417e8fa1090b51.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "991460be503d6e8672e7aab5750755da",
+    "url": "/PortfolioV2.0/index.html"
+  },
+  {
+    "revision": "1deaa3c75721d60e62a1",
+    "url": "/PortfolioV2.0/static/css/main.7010da3b.chunk.css"
+  },
+  {
+    "revision": "45a059b30b3dc2afb332",
+    "url": "/PortfolioV2.0/static/js/2.d203e8a2.chunk.js"
+  },
+  {
+    "revision": "16645896408a141b91c4e8727c0ec2d5",
+    "url": "/PortfolioV2.0/static/js/2.d203e8a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1deaa3c75721d60e62a1",
+    "url": "/PortfolioV2.0/static/js/main.0d30c76d.chunk.js"
+  },
+  {
+    "revision": "6abd0008f189f29cdc50",
+    "url": "/PortfolioV2.0/static/js/runtime-main.06497555.js"
+  },
+  {
+    "revision": "92c7d580d983bc1ba5c9c0a85b898e8c",
+    "url": "/PortfolioV2.0/static/media/BackEndIcon.92c7d580.svg"
+  },
+  {
+    "revision": "be2a454d5839572e197d189910483de4",
+    "url": "/PortfolioV2.0/static/media/ConceptIcon.be2a454d.svg"
+  },
+  {
+    "revision": "9901c4dd3d46a5cd4bc5e3d1c76fa0c8",
+    "url": "/PortfolioV2.0/static/media/FrontEndIcon.9901c4dd.svg"
+  },
+  {
+    "revision": "d22ee3727a7216019c3848df6eafa024",
+    "url": "/PortfolioV2.0/static/media/GitHub.d22ee372.png"
+  },
+  {
+    "revision": "29e3f708fc88d9aa5c47ac197540fa37",
+    "url": "/PortfolioV2.0/static/media/ending.29e3f708.png"
+  },
+  {
+    "revision": "227a2b845ccf5e8e9b8e2d7c1ae528f3",
+    "url": "/PortfolioV2.0/static/media/landingPage.227a2b84.png"
+  }
+]);
