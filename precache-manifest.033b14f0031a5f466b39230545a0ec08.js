ef2d5be5e393751c1659f20f991fef7749d7b6df
Okinz <steven14812@gmail.com>
2021-03-05 01:17:52 -0500

    Updates

---
+++ precache-manifest.033b14f0031a5f466b39230545a0ec08.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "517e6ab013e6158a1f7113a49cb02fcf",
+    "url": "/PortfolioV2.0/index.html"
+  },
+  {
+    "revision": "fbda589b8391c1d690ff",
+    "url": "/PortfolioV2.0/static/css/main.ea75d753.chunk.css"
+  },
+  {
+    "revision": "8db4c2f1e399c6cca539",
+    "url": "/PortfolioV2.0/static/js/2.240ab985.chunk.js"
+  },
+  {
+    "revision": "16645896408a141b91c4e8727c0ec2d5",
+    "url": "/PortfolioV2.0/static/js/2.240ab985.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbda589b8391c1d690ff",
+    "url": "/PortfolioV2.0/static/js/main.28a822f4.chunk.js"
+  },
+  {
+    "revision": "6abd0008f189f29cdc50",
+    "url": "/PortfolioV2.0/static/js/runtime-main.06497555.js"
+  },
+  {
+    "revision": "92c7d580d983bc1ba5c9c0a85b898e8c",
+    "url": "/PortfolioV2.0/static/media/BackEndIcon.92c7d580.svg"
+  },
+  {
+    "revision": "be2a454d5839572e197d189910483de4",
+    "url": "/PortfolioV2.0/static/media/ConceptIcon.be2a454d.svg"
+  },
+  {
+    "revision": "9901c4dd3d46a5cd4bc5e3d1c76fa0c8",
+    "url": "/PortfolioV2.0/static/media/FrontEndIcon.9901c4dd.svg"
+  },
+  {
+    "revision": "d22ee3727a7216019c3848df6eafa024",
+    "url": "/PortfolioV2.0/static/media/GitHub.d22ee372.png"
+  },
+  {
+    "revision": "fdfa559d2179f8598c2075c009e38bc5",
+    "url": "/PortfolioV2.0/static/media/ProjectFolder.fdfa559d.svg"
+  },
+  {
+    "revision": "29e3f708fc88d9aa5c47ac197540fa37",
+    "url": "/PortfolioV2.0/static/media/ending.29e3f708.png"
+  },
+  {
+    "revision": "029ae1098b0121f91f7bf3c63ccc48fa",
+    "url": "/PortfolioV2.0/static/media/github.029ae109.svg"
+  },
+  {
+    "revision": "227a2b845ccf5e8e9b8e2d7c1ae528f3",
+    "url": "/PortfolioV2.0/static/media/landingPage.227a2b84.png"
+  },
+  {
+    "revision": "8002d4bfe0ccff1690e827ea00f095e7",
+    "url": "/PortfolioV2.0/static/media/logo.8002d4bf.png"
+  },
+  {
+    "revision": "b3997ad89ba9097f36f694930aa64890",
+    "url": "/PortfolioV2.0/static/media/share.b3997ad8.svg"
+  }
+]);
